90080f4ab87a38bb5f0164594e2c1ce441adb0b8
AlejoPhoenix <phoenixguitar0@gmail.com>
2014-09-05 11:41:06 -0500

    [ADD] Change for continue in the next exercise.

--- model/attendee.py
+++ model/attendee.py
@@ -3,6 +3,8 @@ from openerp.osv import osv, fields
 
 class Attendee(osv.Model):
 	_name = "openacademy.attendee"
+	_rec_name = "partner_id"
 	_columns = {
-		'name': fields.char(string="Attendee",size=256),
+		'partner_id': fields.many2one('res.partner',string="Partner",required=True,ondelete="cascade"),
+		'session_id':fields.many2one('openacademy.session',string="Session",required=True,ondelete="cascade"),
 	}
