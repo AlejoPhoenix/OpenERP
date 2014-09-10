016496eb588886b55e41776d0e98a24753fdac7a
AlejoPhoenix <phoenixguitar0@gmail.com>
2014-09-10 12:58:02 -0500

    [FIX] Se remueve el campo duplicado que generaba inconsistencia con el trigger del constraint

--- model/session.py
+++ model/session.py
@@ -1,39 +1,72 @@
-# -*- encoding:utf-8 -*-
+# -*- encoding: utf-8 -*-
+
 from openerp.osv import fields, osv
 
 class Session(osv.Model):
-	_name = "openacademy.session"
 
-	def _get_taken_seats_percent(self, seats, attendee_list):
-		try:
-			return(100.0 * len(attendee_list)) / seats
-		except ZeroDivisionError:
-			return 0.0
+    _name = "openacademy.session"
 
-	def _taken_seats_percent(self, cr, uid, ids, field, arg, context=None):
-		result = {}
+    def _get_taken_seats_percent(self, seats, attendee_list):
+        try:
+            return (100.0 * len(attendee_list)) / seats
+        except ZeroDivisionError:
+            return 0.0
 
-		for session in self.browse(cr, uid, ids, context=context):
-			result[session.id] = self._get_taken_seats_percent(session.seats,session.attendee_ids)
-		return result	
+    def _taken_seats_percent(self, cr, uid, ids, field, arg, context=None):
+        result = {}
+        for session in self.browse(cr, uid, ids, context=context):
+            result[session.id] = self._get_taken_seats_percent(session.seats, session.attendee_ids)
+        return result
 
-	_columns = {
-			'name': fields.char(string="Name of session",size=256,help="Please describe correctly waht is the session identifier"),
-			'taken_seats_percent' : fields.function(_taken_seats_percent, type='float', string = "Taken seats"),
-			'start_date': fields.date(string="Start Date", help="Please indicate when the session will start"),
-			'duration': fields.float(string="Duration",digits=(6,2),help="If represent duration in days"),
-			'seats' : fields.integer(string="Seats",help="Total seats availables"),
-			'course_id' : fields.many2one("openacademy.course",'Course',help="Select a course related to this session",ondelete="cascade"),
-			'instructor_id' :fields.many2one("res.partner",string="Instructor",domain="[('instructor','=',True)]"),
-			'attendee_ids' : fields.one2many("openacademy.attendee","session_id",string="Attendees",help="Who will be participated in this ession"),
-	}
+    _columns = {
+        'name': fields.char('Name of Session', 256,
+                help="Please describe correctly what is the session identifier"),        
+        'start_date': fields.date('Start Date',
+                help="Please indicate when the session will start"),
+        'duration': fields.float('Duration', digits=(6,2), help="It represent duration in Days"),
+        'seats': fields.integer('Seats', help="Total seats availables"),
+        'instructor_id' : fields.many2one('res.partner', string="Instructor",
+                                          domain=['|',('instructor','=',True),
+                                          ('category_id.name','ilike','Teacher')]),
+        'course_id': fields.many2one('openacademy.course', 'Course',
+            help="Select a course related to this new session", ondelete="cascade"),
+        'attendee_ids': fields.one2many('openacademy.attendee', 'session_id', 'Attendees',
+            help="Who will be participating in this session"),
+        'taken_seats_percent': fields.function(_taken_seats_percent,
+                                                type='float', string='Taken Seats'),
+    }
 
-	def onchange_taken_seats(self, cr, uid, ids, seats, attendee_ids):
-		attendee_records = self.resolve_2many_commands(cr, uid, 'attendee_ids', attendee_ids,['id'])
-		res = {
-			value : {
-			 	'taken_seats_percent':
-			 		self._get_taken_seats_percent(seats,attendee_records),
-			},
-		}
-		return res
+    def onchange_taken_seats(self, cr, uid, ids, seats, attendee_ids):
+        attendee_records = self.resolve_2many_commands(cr, uid, 'attendee_ids', attendee_ids, ['id'])
+        res = {
+            'value' : {
+                'taken_seats_percent' :
+                    self._get_taken_seats_percent(seats, attendee_records),
+            },
+        }
+        if seats < 0:
+            res['warning'] = {
+                'title'   : "Warning: bad value",
+                'message' : "You cannot have negative number of seats",
+                
+            }
+        elif seats < len(attendee_ids):
+            res['warning'] = {
+                'title'   : "Warning: problems",
+                'message' : "You need more seats for this session",
+                
+            }
+        return res
+
+    def _check_instructor_not_in_attendees(self, cr, uid, ids):
+        for session in self.browse(cr, uid, ids):
+            partners = [att.partner_id for att in session.attendee_ids]
+            if session.instructor_id and session.instructor_id in partners:
+                return False
+        return True
+
+    _constraints = [
+        (_check_instructor_not_in_attendees,
+         "The instructor can not be also an attendee!",
+         ['instructor_id','attendee_ids']),
+    ]
